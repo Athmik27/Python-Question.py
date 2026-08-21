3fa32b4ce2faaf422261a467adaec7babbada287
Athmik A <athmik27@gmail.com>
2026-08-21 21:07:27 +0530

    Refactor ibq2.py with various number operations

--- ibq2.py
+++ ibq2.py
@@ -122,3 +122,182 @@ for i in list_1:
         list_odd.append(i)
 print("even numbers are:",list_even)
 print("odd numbers are:",list_odd)
+
+# #Reverse a list
+numbers = [10, 20, 30, 40, 50]
+rev_num=[]
+for i in numbers:
+    rev_num.insert(0,i)
+print("reversed number is:",rev_num)
+
+sum of all the elements
+numbers = [10, 20, 30, 40, 50]
+total=0
+for i in numbers:
+    total=total+i
+print('sum of the element:',total)
+
+# count numbers of even in list
+numbers = [10, 15, 20, 25, 30, 35, 40]
+count=0
+for i in numbers:
+    if i%2==0:
+        count=count+1
+print("numbers of even are in:",count)
+
+
+# # count numbers of odd in list
+numbers = [10, 15, 20, 25, 30, 35, 40]
+count=0
+for i in numbers:
+    if i%2!=0:
+        count=count+1
+print("numbers of odd are in:",count)
+
+#count number of even and odd
+numbers = [10, 15, 20, 25, 30, 35, 40]
+odd=[]
+even=[]
+for i in numbers:
+    if i%2==0:
+        even.append(i)
+    else :
+        odd.append(i)
+print("even",even)
+print("odd",odd)
+
+#largest in list
+numbers = [10, 25, 7, 45, 18]
+largest=numbers[0]
+for i in numbers:
+    if i>largest:
+        largest=i
+print("largest number is :",largest)
+
+#smallest in list
+numbers = [10, 25, 7, 45, 18]
+smallest=numbers[0]
+for i in numbers:
+    if i<smallest:
+        smallest=i
+print("smallest number is :",smallest)
+
+#number of positive number
+numbers = [-5, 10, -2, 20, 0, 15, -8]
+count=0
+for i in numbers:
+    if i>0:
+        count=count+1
+print("number of positive numbers:",count)
+
+# #number of negative number
+numbers = [-5, 10, -2, 20, 0, 15, -8]
+count=0
+for i in numbers:
+    if i<0:
+        count=count+1
+print("number of negative numbers:",count)
+
+#square of numbers
+numbers = [1, 2, 3, 4, 5]
+sq_num=[]
+for i in numbers:
+    i=i**2
+    sq_num.append(i)
+print('square of numbers:',sq_num)
+
+# #number greater than 20
+numbers = [10, 25, 15, 30, 8, 45]
+num_list=[]
+for i in numbers:
+    if i>20:
+        num_list.append(i)
+print("number grater than 20 is :",num_list)
+
+# #finding the num in list
+numbers = [10, 20, 30, 40, 50]
+num=int(input("enter the number you want to find:"))
+for i in numbers:
+    if i==num:
+        print("number found in list",num)
+
+#remove the duplicate from list
+numbers = [10, 20, 10, 30, 20, 40, 30]
+new_numbers=[]
+for i in numbers:
+    if i not in new_numbers: #not in operator is imp
+        new_numbers.append(i)
+print("new numbers from the original list is:",new_numbers)
+
+#finding second largest numbers
+numbers = [10, 25, 7, 45, 18]
+largest=numbers[0]
+second_largest=numbers[0]
+for i in numbers:
+    if i>largest:
+        second_largest=largest
+        largest=i
+    elif i>second_largest:
+        second_largest=i
+print("second largest number is:",second_largest)
+
+#frequency checking
+numbers = [5, 10, 15, 10, 20]
+count = 0
+for i in numbers:
+    if i==10:
+        count=count+1
+
+ print("10 occurs:", count, "times")
+
+#Sum of only even numbers
+numbers = [10, 15, 20, 25, 30, 35]
+sum=0
+for i in numbers:
+    if i%2==0:
+        sum=sum+i
+print("sum of even numbers is",sum)
+
+# #Sum of only odd numbers
+numbers = [10, 15, 20, 25, 30, 35]
+sum=0
+for i in numbers:
+    if i%2!=0:
+        sum=sum+i
+print("sum of odd numbers is",sum)
+
+#average of list
+numbers = [10, 20, 30, 40, 50]
+total=0
+number=0
+for i in numbers:
+    total=total+i
+    number=number+1
+average=total/number
+print("total ",total)
+print("average ",average)
+
+
+# #Find the number of elements without len()
+numbers = [10, 20, 30, 40, 50, 60, 70]
+number=0
+for i in numbers:
+    number=number+1
+print("number of element:",number)
+
+#Find the sum of elments at even indexes
+numbers = [10, 20, 30, 40, 50, 60]
+sum=0
+# for i in range(len(numbers)):
+    if i%2==0:
+        sum=sum+numbers[i] #here we get the value that is associated to that index
+print("sum of index:",sum)
+
+#Find the sum of elements at odd indexes
+numbers = [10, 20, 30, 40, 50, 60]
+sum=0
+for i in range(len(numbers)):
+    if i%2!=0:
+        sum=sum+numbers[i] #here we get the value that is associated to that index
+print("sum of index:",sum)
+
